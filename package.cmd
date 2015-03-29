49207c6c5a115fc0dab4ce666cfbaf49bb0545db
moljac <mcvjetko@holisticware.net>
2015-03-29 16:32:44 +0200

    Samples Added

--- package.cmd
+++ package.cmd
@@ -4,12 +4,21 @@ set NUGET=.nuget\nuget
 
 %NUGET% ^
 	pack ^
-	"src\Xamarin.Auth.nuspec" ^
+	"src\HolisticWare.Auth.nuspec" ^
 	-Symbols ^
 	-OutputDirectory artifacts ^
 	-Build ^
 	-IncludeReferencedProjects ^
 	-NonInteractive
 
-@IF %ERRORLEVEL% NEQ 0 PAUSE	
+%NUGET% ^
+	pack ^
+	"src\xamarin[]duplo-dep-nugets\Xamarin.Auth.nuspec" ^
+	-Symbols ^
+	-OutputDirectory artifacts ^
+	-Build ^
+	-IncludeReferencedProjects ^
+	-NonInteractive
+
+	@IF %ERRORLEVEL% NEQ 0 PAUSE	
 
