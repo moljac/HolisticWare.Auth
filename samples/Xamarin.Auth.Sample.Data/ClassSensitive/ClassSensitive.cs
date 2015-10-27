59fa93bf73b02568b534ec484fb1d3c0585749a3
moljac <mcvjetko@holisticware.net>
2015-10-27 13:18:32 +0100

    samples    Xamarin.Forms    project references

---
+++ samples/Xamarin.Auth.Sample.Data/ClassSensitive/ClassSensitive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xamarin.Auth.SampleData
+{
+    partial class ClassSensitive<T>
+    {
+        private T data;
+
+        partial void InitializePrivateSecureSecretCritical();
+
+        public ClassSensitive()
+        {
+            // initialize non secure data
+            this.InitializePublicUnSecureNonCritical();
+
+            // initialize secure data
+            // this method will be NOOP-ed 
+            this.InitializePrivateSecureSecretCritical();
+
+
+        }
+    }
+}
